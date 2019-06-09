a0be878b7591f45942e86fc9c4164b3a26ba23f2
ganna <dorogaya_76@list.ru>
2019-06-09 15:06:38 +0000

    Json Star wars table

---
+++ main.js
@@ -0,0 +1,77 @@
+// XMLHttpRequest object is an inbuilt object that JavaScript provides to allow us to consume APIs.
+// So this gives us the method to open connections, to send connections, and close them.
+// var xhr = new XMLHttpRequest();  
+
+
+// Now we want to create a new function: xhr.onreadystatechange().
+// And whenever the state changes of our xhr object, we want to run a check.
+
+// xhr.onreadystatechange = function(){
+//     if(this.readyState == 4 && this.status ==200){
+//         document.getElementById('data').innerHTML=this.responseText;
+//     }
+// };
+// Now that we have this listener waiting to see for xhr's state to change, I need to open a connection  with method GET().
+// xhr.open('GET', 'https://swapi.co/api/');
+// xhr.send();
+function getData(url, cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
+
+function writeToDocument(url) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+
+    getData(url, function(data) {
+        var pagination = "";
+
+        if (data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous);
+        }
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key) {
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15);
+                dataRow.push(`<td>${truncatedData}</td>`);
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
+    });
+}
